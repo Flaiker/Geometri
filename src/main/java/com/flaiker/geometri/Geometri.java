054ea811882c9440b5b519479fd7f5f6e1e8ea6a
Flaiker <flaikerfl@gmail.com>
2014-10-15 14:50:28 +0200

    Initial commit

---
+++ src/main/java/com/flaiker/geometri/Geometri.java
@@ -0,0 +1,31 @@
+package com.flaiker.geometri;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Flaiker on 15.10.2014.
+ */
+@Mod(modid = Geometri.MODID, name = Geometri.MODNAME, version = Geometri.VERSION)
+public class Geometri {
+    public static final String MODID   = "geometri";
+    public static final String MODNAME = "Geometri";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
